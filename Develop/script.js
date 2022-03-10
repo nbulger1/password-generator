ab54afda650ce1b4574acfc69ea55ecd4e3563a8
Natalie Bulger <bulgern20@gmail.com>
2022-03-10 13:56:49 -0800

    Creating guaranteed bucket

--- Develop/script.js
+++ Develop/script.js
@@ -24,6 +24,7 @@ var numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
 
 function generatePassword() {
 
+  //Generate variables that are automatically false unless the user confirms true that they would like one of those characters
   var upperPresence = false;
   var lowerPresence = false;
   var specialPresence = false;
@@ -32,23 +33,30 @@ function generatePassword() {
   let userPassLength = prompt("How long is your desired password? (8-128 characters)", "10");
   //store the password length as a variable
   var passwordLength = userPassLength;
+
   //validation for passwordLength to ensure the entered value is an integer between 8 and 128
-  if(passwordLength < 8){
-    alert("Please enter an integer value between 8 and 128");
-    let userPassLength = prompt("How long is your desired password? (8-128 characters)", "10");
-    //store the password length as a variable
-    passwordLength = userPassLength;
-  } else if(passwordLength > 128) {
-    alert("Please enter an integer value between 8 and 128");
-    let userPassLength = prompt("How long is your desired password? (8-128 characters)", "10");
-    //store the password length as a variable
-    passwordLength = userPassLength;
-  } else if(isNaN(passwordLength)) {
-    alert("Please enter an integer value between 8 and 128");
-    let userPassLength = prompt("How long is your desired password? (8-128 characters)", "10");
-    //store the password length as a variable
-    passwordLength = userPassLength;
-  }
+
+  //Issue - when I answer a letter, number less than 8, or number greater than 128 twice in a row it continues on with the rest of the prompts instead of getting mad again...
+  function passwordValidation() {
+    if(passwordLength < 8){
+      alert("You and I both know that number is less than 8...please try again");
+      let userPassLength = prompt("How long is your desired password? (8-128 characters)", "10");
+      //store the password length as a variable
+      passwordLength = userPassLength;
+    } else if(passwordLength > 128) {
+      alert("You and I both know that number is more than 128...please try again");
+      let userPassLength = prompt("How long is your desired password? (8-128 characters)", "10");
+      //store the password length as a variable
+      passwordLength = userPassLength;
+    } else if(isNaN(passwordLength)) {
+      alert("That's not a number...please try again");
+      let userPassLength = prompt("How long is your desired password? (8-128 characters)", "10");
+      //store the password length as a variable
+      passwordLength = userPassLength;
+    }
+  };
+
+  passwordValidation();
 
   //Ask if the user would like special characters
   let userSpecial = confirm("Would you like special characters?");
@@ -80,7 +88,7 @@ function generatePassword() {
 
   //if they don't select anything alert them to start over
   if(!specialPresence && !numbersPresence && !lowerPresence && !upperPresence){
-    alert("Please select at least one type of characters you desire");
+    alert("Excuse me...you have to pick at least one type of character or this ain't gonna work");
 
     let userSpecial = confirm("Would you like special characters?");
     if(userSpecial) {
@@ -156,103 +164,61 @@ function generatePassword() {
   //Create password array
   var password = [];
 
-  //Locate which pre-created bucket is no longer empty and randomly select characters from that bucket until the length equals the passwordLength determined by the user prompt at the beginning
-  if(everyOption != null){
-    for (var i = 0; i < passwordLength; i++) {
-      password.push(everyOption[Math.floor(Math.random() * everyOption.length)]);
+  //Generate function that locates which pre-created bucket is no longer empty and randomly select characters from that bucket until the length equals the passwordLength determined by the user prompt at the beginning
+  function giveMeRandom(x) {
+    if(x !=null){
+      for (var i = 0; i < passwordLength; i++) {
+        password.push(x[Math.floor(Math.random() * x.length)]);
+      }
     }
-  }
-  
-  if(allButSpecial != null){
-    for (var i = 0; i < passwordLength; i++) {
-      password.push(allButSpecial[Math.floor(Math.random() * allButSpecial.length)]);
-    }
-  }  
+  };
 
-  if(allButNumbers != null){
-    for (var i = 0; i < passwordLength; i++) {
-      password.push(allButNumbers[Math.floor(Math.random() * allButNumbers.length)]);
-    }
-  }  
-  
-  if(allButLower != null){
-    for (var i = 0; i < passwordLength; i++) {
-      password.push(allButLower[Math.floor(Math.random() * allButLower.length)]);
-    }
-  }  
+  //Run the function for each variable to find non-empty bucket  
+  giveMeRandom(everyOption);
+  giveMeRandom(allButSpecial);
+  giveMeRandom(allButNumbers);
+  giveMeRandom(allButLower);
+  giveMeRandom(allButUpper);
+  giveMeRandom(numbersSpecial);
+  giveMeRandom(lowerSpecial);
+  giveMeRandom(numbersLower);
+  giveMeRandom(upperSpecial);
+  giveMeRandom(numbersUpper);
+  giveMeRandom(upperLower);
+  giveMeRandom(justSpecial);
+  giveMeRandom(justLower);
+  giveMeRandom(justUpper);
+  giveMeRandom(justNumbers);
 
-  if(allButUpper != null){
-    for (var i = 0; i < passwordLength; i++) {
-      password.push(allButUpper[Math.floor(Math.random() * allButUpper.length)]);
-    }
-  }  
+  //Create empty bucket for guaranteed characters
+  var guaranteedBucket = [];
 
-  if(numbersSpecial != null){
-    for (var i = 0; i < passwordLength; i++) {
-      password.push(numbersSpecial[Math.floor(Math.random() * numbersSpecial.length)]);
-    }
-  }  
+  //if any of the presence variables are true pull one random character from the associated array and add it to the guaranteed bucket
+  if(upperPresence){
+    guaranteedBucket.push(uppercaseLetters[Math.floor(Math.random() * uppercaseLetters.length)]);
+  };
 
-  if(lowerSpecial != null){
-    for (var i = 0; i < passwordLength; i++) {
-      password.push(lowerSpecial[Math.floor(Math.random() * lowerSpecial.length)]);
-    }
-  }  
+  if(lowerPresence){
+    guaranteedBucket.push(lowercaseLetters[Math.floor(Math.random() * lowercaseLetters.length)]);
+  };
 
-  if(numbersLower != null){
-    for (var i = 0; i < passwordLength; i++) {
-      password.push(numbersLower[Math.floor(Math.random() * numbersLower.length)]);
-    }
-  }  
+  if(numbersPresence){
+    guaranteedBucket.push(numbers[Math.floor(Math.random() * numbers.length)]);
+  };
 
-  if(upperSpecial != null){
-    for (var i = 0; i < passwordLength; i++) {
-      password.push(upperSpecial[Math.floor(Math.random() * upperSpecial.length)]);
-    }
-  }  
+  if(specialPresence){
+    guaranteedBucket.push(specialCharacters[Math.floor(Math.random() * specialCharacters.length)]);
+  };
 
-  if(numbersUpper != null){
-    for (var i = 0; i < passwordLength; i++) {
-      password.push(numbersUpper[Math.floor(Math.random() * numbersUpper.length)]);
-    }
-  }  
+  //replace i amount of characters of password based on guaranteed bucket length
+  for(var i = 0; i < guaranteedBucket.length; i++){
+    password[i] = guaranteedBucket[i].replace();
+  };
 
-  if(upperLower != null){
-    for (var i = 0; i < passwordLength; i++) {
-      password.push(upperLower[Math.floor(Math.random() * upperLower.length)]);
-    }
-  }  
-
-  if(justSpecial != null){
-    for (var i = 0; i < passwordLength; i++) {
-      password.push(justSpecial[Math.floor(Math.random() * justSpecial.length)]);
-    }
-  } 
-
-  if(justLower != null){
-    for (var i = 0; i < passwordLength; i++) {
-      password.push(justLower[Math.floor(Math.random() * justLower.length)]);
-    }
-  } 
-
-  if(justUpper != null){
-    for (var i = 0; i < passwordLength; i++) {
-      password.push(justUpper[Math.floor(Math.random() * justUpper.length)]);
-    }
-  } 
-
-  if(justNumbers != null){
-    for (var i = 0; i < passwordLength; i++) {
-      password.push(justNumbers[Math.floor(Math.random() * justNumbers.length)]);
-    }
-  } 
-
+  //create a password variable that joins the characters of the password array together
   var passwordjoined = password.join('');
-  console.log(passwordjoined);
 
-  //empty array of guaranteed characters - use math.random grab from the bucket they want that was created before this function and add those to the empty array (0-4 depending) - go over the password x times and replace the first x characters with the guaranteed characters (for loop)
-
-  //Take my new passwordjoined variable and returns it in the password generator box on the webpage
+  //return the joined password in the textbox on the webpage
   return passwordjoined;
 
 };
